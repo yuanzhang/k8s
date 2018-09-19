0a1a0a73132bd2d79aa72b4fd5fbfff502716e6b
wzx <wzx@wzxdeMacBook-Pro.local>
2018-09-19 20:24:57 +0800

    vim install update

--- linux_common/bin/vim_install.sh
+++ linux_common/bin/vim_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CUR_DIR=`pwd`
+
 yum remove -y vim
 yum install ncurses-devel -y
 yum install -y python-devel
@@ -10,12 +12,12 @@ cd vim-8.0.0134/
 ./configure --with-features=huge -enable-pythoninterp=yes
 make && make install
 
-
+cp ${CUR_DIR}
 cp ../etc/vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo "alias vi='vim'" >> ~/.bashrc
+echo "alias vi='/usr/local/bin/vim'" >> ~/.bashrc
 source ~/.bashrc
 
 #:PluginInstall
