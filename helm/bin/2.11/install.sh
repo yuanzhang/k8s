f5c925fd17ac56cbd283e96728b730ecc7946a41
wangzhenxing <wzx1987310@gmail.com>
2018-10-23 15:42:22 +0800

    helm install update

--- helm/bin/2.11/install.sh
+++ helm/bin/2.11/install.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# 创建serviceaccount账号
-kubectl create serviceaccount --namespace kube-system tiller
-kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+cp helm /usr/bin -f
 
 # 所有node节点安装socat
 yum install socat -y
 
 helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.11.0 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
 
+echo 'sleeppin 10s ...'
 sleep 10
+
+# 创建serviceaccount账号
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+
 
 helm install stable/nginx-ingress --set rbac.create=true
 
